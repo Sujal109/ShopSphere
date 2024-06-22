7887496fe3d12560f8bcf7817d3876de9fad6d9f
Sujal109 <aggarwalsujal109@gmail.com>
2024-06-22 23:12:38 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
+import cors from 'cors';
 const port = process.env.PORT || 5000 ; 
 
 connectDB(); 
@@ -18,7 +18,9 @@ const app = express();
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
+app.use(cors({
+    origin: 'https://shop-sphere-one.vercel.app/'
+  }));
 //cookie parser middleware
 app.use(cookieParser());
 
