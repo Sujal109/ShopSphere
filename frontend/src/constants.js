b7a2a6edd0f5ab34933424e99efae68fb045dd62
Sujal109 <148677617+Sujal109@users.noreply.github.com>
2024-06-22 23:48:34 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
-export const BASE_URL = '';
+export const BASE_URL = 'https://shop-sphere-backend.vercel.app/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
