9beddf83eb3c01cc2ca466eaac4487261dda6e99
Sujal109 <aggarwalsujal109@gmail.com>
2024-06-24 00:42:59 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const port = process.env.PORT || 5000 ;
 
 connectDB(); 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin : "https://shop-sphere-theta.vercel.app/"
+}));
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
