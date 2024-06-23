3e0989e767f68d690a4ffbd52cdd286d7a7d1acf
Sujal109 <148677617+Sujal109@users.noreply.github.com>
2024-06-24 00:19:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,7 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/', (req, res) => {
-    res.send("API is running");
-    console.log(process.env.PAYPAL_CLIENT_ID);
+    res.send('API is running...')
 });
 
 app.use(notFound);
