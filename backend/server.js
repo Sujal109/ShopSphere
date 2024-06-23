6c9c1a5b17f24923408d22f02e5ed4a47395529b
Sujal109 <aggarwalsujal109@gmail.com>
2024-06-23 22:50:31 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
+import cors from 'cors';
 const port = process.env.PORT || 5000 ; 
 
 connectDB(); 
 const app = express();
-
+app.use(cors());
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
