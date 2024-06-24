3d131f3ee9cd111de3851a8b747f3b888b468f23
Sujal109 <aggarwalsujal109@gmail.com>
2024-06-24 15:26:37 +0530

    merge

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,6 +9,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly:true,
         secure: process.env.NODE_ENV !== 'developement',
+        path:'/',
         sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000 //30days
     });
