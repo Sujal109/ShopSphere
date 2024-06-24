e6e35056dc7b360866533621149b801e3542fb1d
Sujal109 <148677617+Sujal109@users.noreply.github.com>
2024-06-24 15:19:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/', (req, res) => {
-res.send('API is running...');
+res.send(`API is running... ${process.env.JWT_SECRET}`);
 });
 
 app.use(notFound);
